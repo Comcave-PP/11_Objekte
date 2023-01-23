5d59f5c981c848c5829894ca5f5981bd332658d0
Marinus <marinus.fehling@web.de>
2023-01-23 10:24:12 +0100

    nested arrays, nested loop

--- script_01.js
+++ script_01.js
@@ -7,6 +7,28 @@
 // output(arr[0]);
 // output("------------");
 
+// nested arrays [[[]]]
+// hier [[],[]]
+arr =   [
+                ["Ich","bin","Max"],
+                ["Ich","bin","Moritz"]
+        ];
+
+// output(arr);
+// output(arr[0]);
+// output(arr[0][2]);
+// output(arr[1][0]);
+
+// nested loop
+for (let i = 0; i < arr.length; i++)
+{
+        for(let j = 0; j < arr[i].length; j++)
+        {
+                output(arr[i][j]);
+        }
+        
+}
+
 
 /***** Objekte 1 Daten/Funktionen *******/
 
